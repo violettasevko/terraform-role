0d10b21b602d6d8b9a05e94c33ce81556ac6e728
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-08-17 03:07:18 +0300

    init

---
+++ role.tf
@@ -0,0 +1,17 @@
+module "iam_assumable_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+
+  trusted_role_arns = [
+    "arn:aws:iam::280534998596:violetta-sevko",
+  ]
+
+  create_role = true
+
+  role_name         = "testadmin"
+  role_requires_mfa = false
+
+  custom_role_policy_arns = [
+    "arn:aws:iam::aws:policy/AdministratorAccess",
+  ]
+  #number_of_custom_role_policy_arns = 1
+}
